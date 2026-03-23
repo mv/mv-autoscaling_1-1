1432c38c909b51be7f175eb632f427e6aa8632ae
Mv <ferreira.mv@gmail.com>
2026-03-23 11:22:55 -0300

    Helper: user_data is base64 encoded.

---
+++ bin/ec2-get-user-data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+[ "${1}" == "" ] && {
+  echo
+  echo "Usage: $0 <instance-id>"
+  echo
+  exit 1
+}
+
+_instance_id="${1}"
+
+aws ec2 describe-instance-attribute \
+  --instance-id "${_instance_id}"   \
+  --attribute userData --query "UserData.Value" \
+  --output text \
+  | base64 --decode
